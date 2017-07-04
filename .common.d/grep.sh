779365f893cb1a62c55b6271257043cc65c083ef
Mal Graty <mal.graty@googlemail.com>
2017-07-04 14:39:08 +0100

    Update grep options now GREP_OPTIONS is deprecated

--- .common.d/grep.sh
+++ .common.d/grep.sh
@@ -1,8 +1,13 @@
-export GREP_OPTIONS="\
-  -I \
-  --color=auto \
-  --exclude-dir=.git \
-  --exclude-dir=.svn \
-  --exclude-dir=node_modules \
-  --exclude-dir=python_modules \
-"
+export GREP_OPTS='
+  -I
+  --color=auto
+  --exclude-dir=.git
+  --exclude-dir=.svn
+  --exclude-dir=node_modules
+  --exclude-dir=python_modules
+'
+
+grep () {
+  set -- $(echo $GREP_OPTS | tr -d '\n') "$@"
+  /bin/grep "$@"
+}
