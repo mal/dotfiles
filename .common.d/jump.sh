7f2652ce6d06363c574b33d3a7b0baf54d62c0a3
Mal Graty <mal.graty@googlemail.com>
2018-07-12 13:37:03 +0100

    Make jump posix compliant

--- .common.d/jump.sh
+++ .common.d/jump.sh
@@ -2,47 +2,44 @@ export JMP_LIST=""
 export JMP_OPTS=""
 export JMP_PATH=$HOME/code
 
-jmp() {
-    local dir="$(_jmp_list path $1)"
+jmp()
+{
+  local dir="$(_jmp_list path $1)"
 
-    if [ -n "$dir" ]
-        then cd "$dir"; return
-    fi
+  if [ -n "$dir" ]
+    then cd "$dir"; return
+  fi
 
-    local dir="$(_jmp_search -name .git -path "*/$1/.git" -type d | head -n1)"
+  local dir="$(_jmp_search -path "*/$1/.git" | head -n1)"
 
-    if [ -n "$dir" ]
-        then cd "$(dirname "$dir")"
-        else echo "err: unknown jump" >&2; return 1
-    fi
+  if [ -n "$dir" ]
+    then cd "$(dirname "$dir")"
+    else echo "err: unknown jump" >&2; return 1
+  fi
 }
 
-_jmp_completion() {
-    reply=( \
-        $(_jmp_list names) \
-        $(_jmp_search -name .git -type d | xargs -n1 dirname | xargs -n1 basename) \
-    )
+_jmp_list()
+{
+  echo "$JMP_LIST" | tr ':' '\n' | case "$1" in
+    names) cut -d\| -f1 ;;
+    path) grep "^$2|" | cut -d\| -f2 ;;
+  esac
 }
 
-_jmp_list() {
-    case "$1" in
-        names)
-            echo "$JMP_LIST" | tr ':' '\n' | cut -d\| -f1
-            ;;
-        path)
-            echo "$JMP_LIST" | tr ':' '\n' | grep "^$2|" | cut -d\| -f2
-            ;;
-    esac
+_jmp_search()
+{
+  local IFS="$(printf '\nn')"; IFS="${IFS%n}"
+  set -- $(echo $JMP_PATH | tr ':' '\n') $(echo $JMP_OPTS | tr ' ' '\n') \
+    -name .git -type d "$@"
+  unset IFS
+
+  find "$@"
 }
 
-_jmp_search() {
-    local IFS="$(echo \'\\n\')"
-    set -- $(echo $JMP_PATH | tr ':' '\n') $(echo $JMP_OPTS | tr ' ' '\n') "$@"
-    unset IFS
-
-    find "$@"
-}
-
-if type compctl &> /dev/null
-  then compctl -x 'p[1]' -K _jmp_completion -- jmp
+if type compctl > /dev/null 2>&1; then
+  _jmp_comp()
+  {
+    reply=($(_jmp_list names) $(_jmp_search | rev | cut -d/ -f2 | rev))
+  }
+  compctl -x 'p[1]' -K _jmp_comp -- jmp
 fi
